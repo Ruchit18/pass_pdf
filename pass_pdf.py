c0e7928814f0e078c9d7f2673829d29fde99ccab
Ruchit18 <71174915+Ruchit18@users.noreply.github.com>
2021-02-16 09:30:22 +0530

    Add files via upload

---
+++ pass_pdf.py
@@ -0,0 +1,14 @@
+from PyPDF2 import PdfFileReader,PdfFileWriter
+file_pdf=PdfFileReader('123.pdf')#read
+out_pdf=PdfFileWriter()#object for pdf writer
+file_pdf
+
+for i in range(file_pdf.numPages):
+    page_details=file_pdf.getPage(i)#add to the output page
+    out_pdf.addPage(page_details)
+    
+password="qaz@123"
+out_pdf.encrypt(password)
+
+with open("encryptedtickets.pdf","wb") as filename:
+    out_pdf.write(filename)
